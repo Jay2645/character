d5c3f566c87aaa20b3a82df8975901c026db2ae3
Jay Stevens <disneylandjay@gmail.com>
2019-01-20 18:22:14 -0800

    Added state machine nodes.

--- register_types.cpp
+++ register_types.cpp
@@ -1,10 +1,12 @@
 #include "register_types.h"
 
+#include "state_machine_node.h"
 #include "character.h"
 #include "character_movement.h"
 
 void register_character_types() {
 
+	ClassDB::register_class<StateMachineNode>();
 	ClassDB::register_class<Character>();
 	ClassDB::register_class<CharacterMovement>();
 }
